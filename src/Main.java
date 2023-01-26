26c8f0e93860d0dfa7354ddac154737089d63429
Rinat-312 <rkozhobekov88@mail.ru>
2023-01-27 00:29:52 +0300

    2 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
 
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
-//
-//        System.out.println(a + 10);
-//        System.out.println(b - 10);
-
-
-        for (int i = 6; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
